8dff70de9283353440c9ee1348158ac38a50c025
Giorgos Petkakis <85838659+giorgospetkakis@users.noreply.github.com>
2024-04-23 22:40:35 +0200

    Fix and test opposed rolls

--- CallOfCthulu.Tests/SkillCheckTests.cs
+++ CallOfCthulu.Tests/SkillCheckTests.cs
@@ -27,7 +27,40 @@ public class SkillCheckTests
     public void Roll(int skillValue, int roll, RollResult expected)
     {
         Skill skill = new MockSkill(skillValue);
-        RollResult result = Core.Roll(skill, roll);
+        RollResult result = Core.GetRollResult(skill, roll);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void OpposedRoll_WithRandomRolls()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            Skill skill1 = new MockSkill(50);
+            Skill skill2 = new MockSkill(50);
+            int result = Core.GetOpposedRollResult(skill1, skill2, 0, 0, 0, 0);
+            Assert.InRange(result, -1, 1);
+        }
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1, 2, 1)]
+    [InlineData(1, 1, 2, 1, -1)]
+    [InlineData(25, 25, 24, 25, 1)]
+    [InlineData(25, 25, 25, 24, -1)]
+    [InlineData(50, 50, 1, 50, 1)]
+    [InlineData(50, 50, 5, 50, 1)]
+    [InlineData(50, 50, 25, 50, 1)]
+    [InlineData(50, 50, 49, 50, 1)]
+    [InlineData(50, 50, 50, 49, -1)]
+    [InlineData(50, 50, 50, 25, -1)]
+    [InlineData(50, 50, 50, 5, -1)]
+    [InlineData(50, 50, 50, 1, -1)]
+    public void OpposedRoll_WithFixedRolls(int skill1Value, int skill2Value, int roll1, int roll2, int expected)
+    {
+        Skill skill1 = new MockSkill(skill1Value);
+        Skill skill2 = new MockSkill(skill2Value);
+        int result = Core.GetOpposedRollResult(skill1, skill2, roll1, roll2);
         Assert.Equal(expected, result);
     }
 }
--- CallOfCthulu/Core/Core.cs
+++ CallOfCthulu/Core/Core.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace CallOfCthulu
 {
     public enum RollResult
@@ -12,60 +14,10 @@ namespace CallOfCthulu
 
     public static class Core
     {
-        internal static int OpposedRoll((Skill skill, int roll) roll1, (Skill skill, int roll) roll2)
-        {
-            RollResult result1 = Roll(roll1.skill, roll1.roll);
-            RollResult result2 = Roll(roll2.skill, roll2.roll);
-            
-            if (result1 > result2)
-            {
-                return 1;
-            }
-            else if (result1 < result2)
-            {
-                return -1;
-            }
-            else
-            {
-                int extraRoll1;
-                int extraRoll2;
+        public static RollResult GetRollResult(Skill skill, int bonusDice, int penaltyDice)
+            => GetRollResult(skill, Roll(skill, bonusDice, penaltyDice));
 
-                do
-                {
-                    extraRoll1 = Dice.D100.Roll();
-                    extraRoll2 = Dice.D100.Roll();
-                }
-                while(extraRoll1 == extraRoll2);
-
-                return extraRoll1 > extraRoll2 ? 1 : -1;
-            }
-        }
-
-        public static RollResult Roll(Skill skill, int bonusDice, int penaltyDice)
-        {
-            if (bonusDice > 0 && penaltyDice > 0)
-            {
-                throw new ArgumentException("Cannot have both bonus and penalty dice.");
-            }
-
-            if (bonusDice > 0)
-            {
-                int roll = Dice.D100.RollWithBonusDice(bonusDice);
-                return Roll(skill, roll);
-            }
-            else if (penaltyDice > 0)
-            {
-                int roll = Dice.D100.RollWithPenaltyDice(penaltyDice);
-                return Roll(skill, roll);
-            }
-            else
-            {
-                int roll = Dice.D100.Roll();
-                return Roll(skill, roll);
-            }
-        }
-
-        internal static RollResult Roll(Skill skill, int roll)
+        public static RollResult GetRollResult(Skill skill, int roll)
         {
             if ((skill.Value >= 50 && roll == 100) || (skill.Value < 50 && roll >= 96))
             {
@@ -94,6 +46,60 @@ namespace CallOfCthulu
             else
             {
                 throw new Exception($"Could not determine roll result for skill {skill} and roll {roll}.");
+            }
+        }
+
+        public static int GetOpposedRollResult(Skill skill1, Skill skill2, int bonusDice1, int bonusDice2, int penaltyDice1, int penaltyDice2)
+        {
+            int roll1 = Roll(skill1, bonusDice1, penaltyDice1);
+            int roll2 = Roll(skill2, bonusDice2, penaltyDice2);
+
+            return GetOpposedRollResult(skill1, skill2, roll1, roll2);
+        }
+
+        internal static int GetOpposedRollResult(Skill skill1, Skill skill2, int roll1, int roll2)
+        {
+            RollResult result1 = GetRollResult(skill1, roll1);
+            RollResult result2 = GetRollResult(skill2, roll2);
+
+            if (result1 == result2)
+            {
+                while(roll1 == roll2)
+                {
+                    roll1 = Roll(skill1, 0, 0);
+                    roll2 = Roll(skill2, 0, 0);
+                }
+
+                return roll1 < roll2 ? 1 : -1;
+            }
+            else if (result1 > result2)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        internal static int Roll(Skill skill, int bonusDice, int penaltyDice)
+        {
+            if (bonusDice > 0 && penaltyDice > 0)
+            {
+                throw new ArgumentException("Cannot have both bonus and penalty dice.");
+            }
+
+            if (bonusDice > 0)
+            {
+                return Dice.D100.RollWithBonusDice(bonusDice);
+            }
+            else if (penaltyDice > 0)
+            {
+                return Dice.D100.RollWithPenaltyDice(penaltyDice);
+            }
+            else
+            {
+                return Dice.D100.Roll();
             }
         }
     }
